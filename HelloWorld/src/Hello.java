bc53ac7649c16f5f6c904ec1257b0776b96680a4
Bruce MacLeod <macleod@maine.edu>
2016-02-02 15:55:04 -0500

    Reported memory statistics. Closes #2

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,30 @@ import java.util.Date;
 
 public class Hello {
 
+	public static void displayMemory() 
+	{
+		int mb = 1024*1024;
+        
+        //Getting the runtime reference from system
+        Runtime runtime = Runtime.getRuntime();
+         
+        System.out.println("##### Heap utilization statistics [MB] #####");
+         
+        //Print used memory
+        System.out.println("Used Memory:"
+            + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+ 
+        //Print free memory
+        System.out.println("Free Memory:"
+            + runtime.freeMemory() / mb);
+         
+        //Print total available memory
+        System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+ 
+        //Print Maximum available memory
+        System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+	}
+	
 	public static void displayDate() 
 	{
 		DateFormat dateFormat = new SimpleDateFormat(" yyyy/MM/dd");
@@ -16,6 +40,7 @@ public class Hello {
 		PreAmble.amble();
 		System.out.println("Hello, My name is Bruce B MacLeod");
 		displayDate();
+		displayMemory();
 		PostAmble.sayGoodBye();
 	}
 
