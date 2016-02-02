bbfede48ff35c2feb01504d087bc4dd114ccf463
Bruce MacLeod <macleod@maine.edu>
2016-02-02 15:43:40 -0500

    Provided code for reporting date

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,11 +1,20 @@
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Hello {
 
+	public static void displayDate() 
+	{
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date)); //2014/08/06
+	}
 	public static void main(String[] args) 
 	{
 		PreAmble.amble();
 		System.out.println("Hello, My name is Bruce B MacLeod");
+		displayDate();
 		PostAmble.sayGoodBye();
 	}
 
